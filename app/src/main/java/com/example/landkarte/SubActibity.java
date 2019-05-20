fc5b1ea00da4978142d694769c2914e20acc3906
ryuuichi <alphabetaganmaderuta@gmail.com>
2019-05-20 23:59:05 +0900

    sub-activity

---
+++ app/src/main/java/com/example/landkarte/SubActibity.java
@@ -0,0 +1,13 @@
+package com.example.landkarte;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SubActibity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub_actibity);
+    }
+}
